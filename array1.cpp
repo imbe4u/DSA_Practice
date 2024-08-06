e5c41bb5bc339d423553d1f291c5f5921c6788db
Anshu Singh <anshuonwork@gmail.com>
2024-08-06 12:57:50 +0530

    origin main

--- array1.cpp
+++ array1.cpp
@@ -1,2 +1,33 @@
-#include <iostream>
+//1. Q - Program to find sum of elements in given array
+
+#include <bits/stdc++.h>
 using namespace std;
+    int sum(vector<int> arr){
+        int sum = 0;
+        for(int i= 0; i <arr.size(); i++){
+            sum += arr[i];
+        }
+        return sum;
+    }
+int main(){
+    vector<int>arr;
+    //int val,n;
+    //cin>>n;
+    // for(int i=0;i<n;i++){
+    //     cin>>val;
+    //     arr.push_back(val);
+    // }
+//^ it was statically by giving the size of array 
+
+//it is done dynamically but for that we have to give -1 value to stop the loop
+int val;
+while(true){
+  cin>>val;
+    if(val == -1){
+      break;
+      }
+         arr.push_back(val);
+}
+int ans=sum(arr);
+cout<<ans;
+}
